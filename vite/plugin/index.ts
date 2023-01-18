242c4bb528b2e259fff4ab7d96c0136fced3fa12
yeyanghui <yeyanghui2022@163.com>
2023-01-18 11:20:22 +0800

    feat: 引入mockjs

--- vite/plugin/index.ts
+++ vite/plugin/index.ts
@@ -1,9 +1,10 @@
 
 import vue from '@vitejs/plugin-vue'
 import { Plugin } from 'vite'
-
+import { setupMockPlugin } from './mock'
 
 export function setupPlugins(isBuild:boolean,env:Record<string, any>){
     const plugins : Plugin[] = [vue()]
+    plugins.push(setupMockPlugin(isBuild))
     return plugins
 }
